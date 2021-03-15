55293197fde358c5ea57203d2d63c5db24be633d
6320502479 <pattrapol.J@ku.th>
2021-03-15 18:03:38 +0700

    p

--- lab6_2.c
+++ lab6_2.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+int main()
+{
+    int n,i,t,f;
+    scanf("%d",&n);
+    char num[2][n];
+    for(i=0;i<2;i++)
+    {
+        scanf("%s",num[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(num[0][i]==num[1][i])
+        {
+            t++;
+        }
+    }
+    f=n-t;
+    printf("%d %d",t,f);
+    return 0;
+}
