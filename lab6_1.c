9f5dd1204d58d9c00b16c0eab54c62d20bb959bf
6320502479 <pattrapol.J@ku.th>
2021-03-15 19:25:18 +0700

    d

--- lab6_1.c
+++ lab6_1.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int n,i;
+    int n,i,j;
     scanf("%d",&n);
-    char word[n][10];
+    char word[n][10],sort[100];
     for(i=0;i<n;i++)
     {
         scanf("%s",&word[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+            if(word[i][0]>word[i+1][0])
+            {
+                strcpy(sort,word[i+1]);
+                strcpy(word[i],word[i+1]);
+                strcpy(word[i],sort);
+            }
     }
     for(i=0;i<n;i++)
     {
